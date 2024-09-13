f04ab1ce12c1993e77841dac2850b677a2028430
fcelas <filipecsaraiva@gmail.com>
2024-09-13 12:12:37 -0300

    adicionadas novas funções, blocklist agora possui limite de 12h

--- src/js-fw.js
+++ src/js-fw.js
@@ -7,19 +7,18 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-const blocklist = fs.readFileSync('blocklist.txt', 'utf-8').split('\n').map(ip => ip.trim());
-const allowlist = fs.readFileSync('allowlist.txt', 'utf-8').split('\n').map(ip => ip.trim());
+let blocklist = new Map();
+let allowlist = new Map();
+//const allowlist = fs.readFileSync('allowlist.txt', 'utf-8').split('\n').map(ip => ip.trim());
 
 let bloquearHTTP = false;
 let bloquearMetodos = false;
 let bloquearGeoSuspeito = false;
 let resultados = [];
 
-//listas de métodos e países de origem suspeitos
 const metodosSuspeitos = ['PUT', 'DELETE', 'PATCH', 'OPTIONS']
 const paisesSuspeitos = ['cn']
 
-//regex de ataques
 const regexMap = [
     { regex: /\/";!--"<XSS>=&\{\(\)\}/, descricao: 'ataque XSS' },
     { regex: /\/%00%01%02%03%04%05%06%07/, descricao: 'URL Encoding' },
@@ -36,16 +35,91 @@ const regexMap = [
     { regex: /<script>alert\(['"]XSS['"]\)<\/script>/, descricao: 'ataque XSS via script' }
 ];
 
-//função que analisa tráfego
-function analisaTrafego(ip, scheme, metodo, pais, path) {
+function adicionarNaBlocklist(ip) {
+    const agora = Date.now();
+    blocklist.set(ip, agora);
+    salvarBlocklist();
+    console.log(`IP ${ip} adicionado à blocklist temporária.`);
+}
+
+function estaNaBlocklist(ip) {
+    if (blocklist.has(ip)) {
+        const tempoAdicionado = blocklist.get(ip);
+        const agora = Date.now();
+        const diferencaTempo = agora - tempoAdicionado;
+        const dozeHorasEmMs = 12 * 60 * 60 * 1000;
+
+        if (diferencaTempo < dozeHorasEmMs) {
+            return true;
+        } else {
+            blocklist.delete(ip);
+            salvarBlocklist();
+            console.log(`IP ${ip} removido da blocklist temporária após 12 horas.`);
+            return false;
+        }
+    }
+    return false;
+}
+
+function limparBlocklistExpirada() {
+    const agora = Date.now();
+    const dozeHorasEmMs = 12 * 60 * 60 * 1000;
+
+    for (let [ip, tempo] of blocklist) {
+        if (agora - tempo >= dozeHorasEmMs) {
+            blocklist.delete(ip);
+            console.log(`IP ${ip} removido da blocklist temporária durante limpeza.`);
+        }
+    }
+    salvarBlocklist();
+}
+
+function salvarBlocklist() {
+    const dados = JSON.stringify(Array.from(blocklist.entries()));
+    fs.writeFileSync('blocklist_temp.json', dados);
+}
+
+function carregarBlocklist() {
+    try {
+        const dados = fs.readFileSync('blocklist_temp.json', 'utf-8');
+        blocklist = new Map(JSON.parse(dados));
+        console.log('Blocklist temporária carregada do arquivo.');
+    } catch (error) {
+        console.log('Arquivo de blocklist não encontrado. Criando uma nova blocklist.');
+    }
+}
+
+function adicionarNaAllowList(ip) {
+    const agora = Date.now();
+    allowlist.set(ip, agora);
+    salvarAllowlist();
+    console.log(`IP ${ip} adicionado à allowlist.`);
+}
+
+function salvarAllowlist() {
+    const dados = JSON.stringify(Array.from(allowlist.entries()));
+    fs.writeFileSync('allowlist.json', dados);
+}
+
+function carregarAllowlist() {
+    try {
+        const dados = fs.readFileSync('allowlist.json', 'utf-8');
+        blocklist = new Map(JSON.parse(dados));
+        console.log('Allowlist carregada do arquivo.');
+    } catch (error) {
+        console.log('Arquivo de allowlist não encontrado. Criando uma nova allowlist.');
+    }
+}
+
+function analisaTrafego(ip, scheme, metodo, pais, path, bytes) {
     let resultado = { status: '', ip: ip, scheme: scheme, metodo: metodo, pais: pais, severidade: '0', path: path };
 
-    if (blocklist.includes(ip)) {
-        resultado.status = 'Tráfego bloqueado: blocklist';
-    } else if (allowlist.includes(ip)) {
+    if (estaNaBlocklist(ip)) {
+        resultado.status = 'Tráfego bloqueado: blocklist temporária';
+        resultado.severidade = '4';
+    } else if (allowlist.has(ip)) {
         resultado.status = 'Tráfego permitido: allowlist';
     } else {
-
         if (!bloquearHTTP && scheme === 'http') {
             resultado.status = 'Tráfego HTTP permitido';
             resultado.severidade = '3'
@@ -75,30 +149,45 @@ function analisaTrafego(ip, scheme, metodo, pais, path) {
             resultado.status = `Tráfego bloqueado: ${ataqueDetectado.descricao}`;
             resultado.severidade = '4';
         }
+
+        if (bytes > 4000) {
+            resultado.severidade = '4'
+            resultado.status = `Tráfego bloqueado:  requisição de ${bytes}B excede limite máximo de tamanho`
+        }
     }
 
     resultados.push(resultado);
+    logAcao(resultado);
+}
+
+function logAcao(resultado) {
+    const logEntry = `${new Date().toISOString()} - IP: ${resultado.ip}, Status: ${resultado.status}, Severidade: ${resultado.severidade}\n`;
+    fs.appendFileSync('firewall_log.txt', logEntry);
 }
 
 function perguntas() {
+    rl.question('Deseja bloquear requisições HTTP? (s/n): ', (answerHTTP) => {
+        if (answerHTTP.toLowerCase() === 's') {
+            bloquearHTTP = true;
+        } 
 
-rl.question('Deseja bloquear requisições HTTP? (s/n): ', (answerHTTP) => {
-    if (answerHTTP.toLowerCase() === 's') {
-        bloquearHTTP = true;
-    } 
-
-    rl.question('Bloquear métodos suspeitos? (s/n): ', (answerMetodos) => {
-        if (answerMetodos.toLowerCase() === 's') {
-            bloquearMetodos = true;
-        }
-
-        rl.question('Bloquear Geolocalização suspeita? (s/n): ', (answerGeo) => {
-            if (answerGeo.toLowerCase() === 's') {
-                bloquearGeoSuspeito = true;
+        rl.question('Bloquear métodos suspeitos? (s/n): ', (answerMetodos) => {
+            if (answerMetodos.toLowerCase() === 's') {
+                bloquearMetodos = true;
             }
-        
-    
 
+            rl.question('Bloquear Geolocalização suspeita? (s/n): ', (answerGeo) => {
+                if (answerGeo.toLowerCase() === 's') {
+                    bloquearGeoSuspeito = true;
+                }
+            
+                processarTrafego();
+            });
+        });
+    });
+}
+
+function processarTrafego() {
     fs.createReadStream('test-dataset.csv')
         .pipe(csv())
         .on('data', (row) => {
@@ -107,21 +196,59 @@ rl.question('Deseja bloquear requisições HTTP? (s/n): ', (answerHTTP) => {
             const metodo = row.ClientRequestMethod.toUpperCase();
             const pais = row.ClientCountry.toLowerCase();
             const path = row.ClientRequestPath;
-            analisaTrafego(ipOrigem, scheme, metodo, pais, path);
+            const bytes = row.ClientRequestBytes;
+            analisaTrafego(ipOrigem, scheme, metodo, pais, path, bytes);
         })
         .on('end', () => {
             fs.writeFileSync('analise_trafego.json', JSON.stringify(resultados, null, 2));
-
             console.log('Análise de tráfego concluída. Resultados salvos no arquivo analise_trafego.json.');
-            rl.close();
+            perguntarAdicionarIP();
+            perguntarAdicionarIPAllow()
         });
-    })
-    })
-});
-
 }
 
+let intervaloLimpeza;
+
+function iniciarLimpezaPeriodica() {
+    intervaloLimpeza = setInterval(limparBlocklistExpirada, 60 * 60 * 1000);
+}
+
+function pararLimpezaPeriodica() {
+    if (intervaloLimpeza) {
+        clearInterval(intervaloLimpeza);
+    }
+}
+function perguntarAdicionarIP() {
+    rl.question('Deseja adicionar algum IP à blocklist? (s/n): ', (resposta) => {
+        if (resposta.toLowerCase() === 's') {
+            rl.question('Digite o IP que deseja adicionar à blocklist: ', (ip) => {
+                adicionarNaBlocklist(ip);
+                perguntarAdicionarIP(); 
+            });
+        } else {
+            pararLimpezaPeriodica();
+            rl.close();
+ 
+        }
+    });
+}
+
+function perguntarAdicionarIPAllow() {
+    rl.question('Deseja adicionar algum IP à Allowlist? (s/n): ', (resposta) => {
+        if (resposta.toLowerCase() === 's') {
+            rl.question('Digite o IP que deseja adicionar à allowlist: ', (ip) => {
+                adicionarNaAllowList(ip);
+                perguntarAdicionarIPAllow(); 
+            });
+        } else {
+            console.log('Análise finalizada. Encerrando o programa.');
+            rl.close();
+            process.exit(0); 
+        }
+    });
+}
+
+carregarAllowlist()
+carregarBlocklist();
+iniciarLimpezaPeriodica(); 
 perguntas();
-
-
-
